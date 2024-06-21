e7112719a9ab24c247ca5fb19a44243df3ea7807
300ST <peerapatpimloy@gmail.com>
2024-06-21 12:07:16 +0700

    Create 65119711-4.dart

---
+++ 65119711-4.dart
@@ -0,0 +1,14 @@
+int factorial(int n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+void main() {
+  int n = 7;
+
+  int result = factorial(n);
+  print("The factorial of $n is: $result");
+}
